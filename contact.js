9ac28a7631b7029276f0c01f67ed9ca2110a7505
waph-wilkiejj <wilkiejj@mail.uc.edu>
2026-06-10 19:32:41 +0000

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,12 +1,12 @@
 var shown = false;
-			function showHideContact(){
-				if (shown){
-					document.getElementById('contact').innerHTML = "Show Contact Information";
-					shown = false;
-				}else{
-					var emailAddress = "<a href='mailto:wilkiejj" + "@" + "mail.uc.edu'>wilkiejj" + "@" + "mail.uc.edu</a>";
- 				   	var phoneNumber = "<a href='tel:" + "+" + "15024159814'>" + "+" + "1 502-415-9814</a>";
-					document.getElementById('contact').innerHTML= "Email: " + emailAddress + " | Phone: " + phoneNumber;
-					shown = true;
-				}
-			}
+	function showHideContact(){
+		if (shown){
+			document.getElementById('contact').innerHTML = "Show Contact Information";
+			shown = false;
+		}else{
+			var emailAddress = "<a href='mailto:wilkiejj" + "@" + "mail.uc.edu'>wilkiejj" + "@" + "mail.uc.edu</a>";
+ 		   	var phoneNumber = "<a href='tel:" + "+" + "15024159814'>" + "+" + "1 502-415-9814</a>";
+			document.getElementById('contact').innerHTML= "Email: " + emailAddress + " | Phone: " + phoneNumber;
+			shown = true;
+		}
+	}
